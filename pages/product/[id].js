8a12e60c32def482f3734be627824f0dc5c115fb
Snowkids-code <oumaj47@gmail.com>
2022-10-25 18:38:18 +0300

    single product page responsive

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -13,7 +13,7 @@ const Product = ({ pizza }) => {
   const [extras, setExtras] = useState([]);
 
   //quantity
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
   const handleSize = (sizeIndex) => {
     const difference = pizza.prices[sizeIndex] - pizza.prices[size];
@@ -39,74 +39,85 @@ const Product = ({ pizza }) => {
 
   const dispatch = useDispatch();
 
-  const handleClick= () => {
-    dispatch(addProduct({...pizza,extras,price,quantity}))
-  }
+  const handleClick = () => {
+    dispatch(addProduct({ ...pizza, extras, price, quantity }));
+  };
 
   return (
     <div className="product_container">
-      <div className="product_left">
-        <div className="product_image_container">
-          <Image
-            src={pizza.img}
-            alt="image"
-            layout="fill"
-            objectFit="contain"
-          />
-        </div>
-      </div>
-      <div className="product_right">
-        <h1 className="product_title">{pizza.title}</h1>
-        <span className="product_price">${price}</span>
-        <p className="product_description">{pizza.desc}</p>
-        <h3>Chose your size</h3>
-        <div className="product_sizes">
-          <div className="product_size" onClick={() => handleSize(0)}>
+      <div className="product_wrapper">
+        <div className="product_left">
+          <div className="product_image_container">
             <Image
-              src="https://raw.githubusercontent.com/safak/youtube/next-pizza-ui/public/img/size.png"
-              layout="fill"
+              src={pizza.img}
               alt="image"
+              layout="fill"
+              height={200}
+              width={200}
               objectFit="contain"
             />
-            <span className="product_number">Small</span>
-          </div>
-          <div className="product_size" onClick={() => handleSize(1)}>
-            <Image
-              src="https://raw.githubusercontent.com/safak/youtube/next-pizza-ui/public/img/size.png"
-              layout="fill"
-              alt="image"
-              objectFit="contain"
-            />
-            <span className="product_number">Medium</span>
-          </div>
-          <div className="product_size" onClick={() => handleSize(2)}>
-            <Image
-              src="https://raw.githubusercontent.com/safak/youtube/next-pizza-ui/public/img/size.png"
-              layout="fill"
-              alt="image"
-              objectFit="contain"
-            />
-            <span className="product_number">Large</span>
           </div>
         </div>
-        <h3>Chose additional ingredients</h3>
-        <div className="product_ingredients">
-          {pizza.extraOptions.map((option) => (
-            <div className="product_option" key={option._id}>
-              <input
-                type="checkbox"
-                id={option.text}
-                name={option.text}
-                className="product_ingredients_checkbox"
-                onChange={(e) => handleChange(e, option)}
+        <div className="product_right">
+          <h1 className="product_title">{pizza.title}</h1>
+          <span className="product_price">${price}</span>
+          <p className="product_description">{pizza.desc}</p>
+          <h3>Chose your size</h3>
+          <div className="product_sizes">
+            <div className="product_size" onClick={() => handleSize(0)}>
+              <Image
+                src="https://raw.githubusercontent.com/safak/youtube/next-pizza-ui/public/img/size.png"
+                layout="fill"
+                alt="image"
+                objectFit="contain"
               />
-              <label htmlFor="double">{option.text}</label>
+              <span className="product_number">Small</span>
             </div>
-          ))}
-        </div>
-        <div className="product_add">
-          <input onChange={(e) => setQuantity(e.target.value)} type="number" defaultValue={1} className="product_quantity" />
-          <button className="product_cart" onClick={handleClick}>Add to cart</button>
+            <div className="product_size" onClick={() => handleSize(1)}>
+              <Image
+                src="https://raw.githubusercontent.com/safak/youtube/next-pizza-ui/public/img/size.png"
+                layout="fill"
+                alt="image"
+                objectFit="contain"
+              />
+              <span className="product_number">Medium</span>
+            </div>
+            <div className="product_size" onClick={() => handleSize(2)}>
+              <Image
+                src="https://raw.githubusercontent.com/safak/youtube/next-pizza-ui/public/img/size.png"
+                layout="fill"
+                alt="image"
+                objectFit="contain"
+              />
+              <span className="product_number">Large</span>
+            </div>
+          </div>
+          <h3>Chose additional ingredients</h3>
+          <div className="product_ingredients">
+            {pizza.extraOptions.map((option) => (
+              <div className="product_option" key={option._id}>
+                <input
+                  type="checkbox"
+                  id={option.text}
+                  name={option.text}
+                  className="product_ingredients_checkbox"
+                  onChange={(e) => handleChange(e, option)}
+                />
+                <label htmlFor="double">{option.text}</label>
+              </div>
+            ))}
+          </div>
+          <div className="product_add">
+            <input
+              onChange={(e) => setQuantity(e.target.value)}
+              type="number"
+              defaultValue={1}
+              className="product_quantity"
+            />
+            <button className="product_cart" onClick={handleClick}>
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
